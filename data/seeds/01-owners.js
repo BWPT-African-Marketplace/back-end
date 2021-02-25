9e72ad258ee00c61cc841b07039c178aff5ac660
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-25 00:40:47 -0500

    start npm file added for heroku

--- data/seeds/01-owners.js
+++ data/seeds/01-owners.js
@@ -1,13 +1,8 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  return knex('owners').insert([   
+    { id:1 ,username: 'admin1', password:'admin1'},
+    { id: 2, username: 'admin2', password:'admin2' },
+    { id:3 ,username: 'admin3', password:'admin3'}
+  ]);
 };
