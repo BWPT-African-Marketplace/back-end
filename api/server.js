afb8cbd36710aaeea08fec8174d4aec40239a0e7
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-03-01 21:39:32 -0500

    cors installed

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const server = express();
 const AuthRouter = require('../api/auth/auth-routers')
 const OwnersRouter = require('../api/owners/owners-router')
 const ItemRouter = require('../api/items/items-router')
 server.use(express.json());
+server.use(cors());
 server.use('/api/items',ItemRouter);
 server.use('/api', AuthRouter);
 server.use('/api/owners', OwnersRouter);
